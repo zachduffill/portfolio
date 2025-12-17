ff71584098aabebb05a2f2ecc4c40dfa974e7576
zachduffill <zach.duffill@gmail.com>
2025-12-17 17:42:23 +0100

    started reworking banner

---
+++ src/lib/Banner.svelte
@@ -0,0 +1,41 @@
+<script>
+    function switchLanguage() {
+        if (window.location.pathname.startsWith('/hu')) {
+            window.location.href = '/';
+        } else {
+            window.location.href = '/hu';
+        }
+    }
+</script>
+
+<style>
+    .banner{
+        width: 100vw;
+        height: 10vh;
+        position:absolute;
+        top: 0;
+        left: 0;
+        user-select:none;
+        pointer-events: none;
+    }
+
+    #banner {
+        background-color: white;
+    }
+
+    #banner-overlay{
+        pointer-events: none;
+        box-shadow: inset 0px 0px 20px rgba(0,0,0,0.6);
+    }
+
+    #tile-grid, #space-grid {
+        overflow: hidden;
+    }
+
+</style>
+
+<div class="banner" id="banner">
+</div>
+<div class="banner" id="banner-overlay"></div>
+<div class="banner" id="tile-grid"></div>
+<div class="banner" id="space-grid"></div>

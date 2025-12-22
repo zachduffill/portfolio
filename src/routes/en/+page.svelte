0ba8fba08270d7195c9bdca88eb0bccb67e04870
zachduffill <zach.duffill@gmail.com>
2025-12-22 22:04:45 +0100

    under construction gif

--- src/routes/en/+page.svelte
+++ src/routes/en/+page.svelte
@@ -4,5 +4,16 @@
     import Statusbar from '$lib/Statusbar.svelte';
 </script>
 
+<style>
+    img {
+        position: absolute;
+        
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+</style>
+
 <Banner language="en"/>
 <Statusbar language="en"/>
+<img src="https://web.archive.org/web/20091025145432im_/http://geocities.com/Area51/Nova/1466/underconstruction.gif" alt="">

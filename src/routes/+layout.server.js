b2ed265a958e7ef9f9c8b996c872d1d680218c97
zachduffill <zach.duffill@gmail.com>
2026-03-17 13:16:56 +0100

    server logic for /pdf

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -3,6 +3,11 @@ import { redirect } from '@sveltejs/kit';
 export function load({ url }) {
     const path = url.pathname;
 
+    if (path == "/cv")
+    {
+        throw redirect(307, '/cv.pdf');
+    }
+
     if (!path.startsWith('/en') && !path.startsWith('/hu'))
     {
         throw redirect(307, '/en'+path);
